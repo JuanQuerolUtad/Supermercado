fddce6e07c5ff8300ecf3cec36c08159fd80d36e
Juan Querol Leon <juan.querol@live.u-tad.com>
2023-01-20 18:26:41 +0100

    modificacion3 Supermercado

--- supermercado/src/casosupuesto/SupermercadoMain.java
+++ supermercado/src/casosupuesto/SupermercadoMain.java
@@ -15,61 +15,105 @@ public class SupermercadoMain {
 		TreeSet<String> productoVenta = new TreeSet<>();
 		productoVenta.addAll(Arrays.asList(arrayComida));
 		TreeSet<String>productoCarrito = new TreeSet<>();
-		switch (numMenu) {
-		case 1:
-			Personal(productoVenta,productoCarrito,numMenu2);
-		case 2:
+		System.out.println("  ***********MENÚ*******");
+		System.out.println(" ");
+		System.out.println("1. Personal");
+		System.out.println("2. Cliente");
+		numMenu = src.nextInt();
+		if (numMenu == 1) {
+			contraseña(productoVenta,productoCarrito,numMenu2);
+		} else if(numMenu==2){
 			Cliente(productoVenta,productoCarrito,numMenu2);
+		}else {
+			System.out.println("Error");
 		}
 		
 		
 		
+		
 	}
+	public static void contraseña(TreeSet<String> productoVenta, TreeSet<String> productoCarrito, int numMenu2) {
+		String clave = "";
+		int cont =0;
+		while(cont != 3 || !clave.equals("J4v4d0n4")) {
+			
+			System.out.println("Escriba la contraseña:");
+			cont++;
+			clave = src.nextLine();
+			if (cont == 2 && cont ==3) {
+				System.out.println("cotraseña incorecta intentelo de nuevo.");
+			}
+			
+	}
+	if (clave.equals("J4v4d0n4")) {
+		Personal(productoVenta,productoCarrito,numMenu2);	
+	}else {
+		System.out.println("Error acceso denegado intentelo mas tar");
+	}
+	
+}
 	public static void Personal(TreeSet<String> productoVenta, TreeSet<String> productoCarrito, int numMenu2) {
-		do {
+		
+		numMenu2 = 0;
+		while (numMenu2 <=4 && numMenu2 >=0){
 			imprimirMenuPersonal();
 			numMenu2 = src.nextInt();
-			switch(numMenu2) {
-			case 1:
+			if (numMenu2 ==1) {
 				System.out.println("Estos son los productos que estan a la venta:" );
 				System.out.println(productoVenta);
-			case 2:
-				
+			}else if(numMenu2==2){
 				productoVenta.add(src.nextLine());
-			case 3:
+			}else if(numMenu2==3){
 				productoVenta.remove(src.nextLine());
-			case 4:
-				consultaCarrito(productoCarrito);
-			case 5:
+			}else if(numMenu2==4){
+				
+			}else if(numMenu2==5){
 				System.out.println("Has terminado la primera parte");
-			default:
+			}else {
 				System.out.println("Error. No existe esa opcion");
 			}
-		} while (numMenu2 >=4 && numMenu2 <=1);
+			
+		} 
+		
+	}
+	public static void ModificarProducto(TreeSet<String> productoCarrito,TreeSet<String> productoVenta) {
+		System.out.println("Escribe producto que quires Modificar en el carrito");
+		String ProductoSacado = src.nextLine();
+		System.out.println("Escribe producto que quieres sustituir  en el carrito");
+		String ProductoCambio = src.nextLine();
+		
+		if (productoVenta.contains(ProductoCambio)) {
+			productoVenta.remove(ProductoSacado);
+			productoVenta.add(ProductoCambio);
+			System.out.println("cambio finalizado corectamente.");
+		}else {
+			System.out.println("el producto no esta");
+		}
 	}
 	public static void Cliente (TreeSet<String> productoVenta, TreeSet<String> productoCarrito, int numMenu2) {
-		do {
+		numMenu2=0;
+		while (numMenu2 >=0 && numMenu2<=5){
 			imprimirMenu();
 			numMenu2 = src.nextInt();
-			switch(numMenu2) {
-			case 1:
+			if (numMenu2==1) {
 				System.out.println("Estos son los productos que estan a la venta:" );
 				System.out.println(productoVenta);
-			case 2:
+			}else if(numMenu2==2) {
 				rellenarCarrito(productoCarrito,productoVenta);
-			case 3:
+			}else if(numMenu2==3) {
 				System.out.println("Estos son los productos que tienes en el carrito:" );
 				System.out.println(productoCarrito);
-			case 4:
+			}else if(numMenu2==4) {
 				consultaCarrito(productoCarrito);
-			case 5:
+			}else if(numMenu2==5) {
 				cambioProducto(productoCarrito,productoVenta);
-			case 6:
+			}else if(numMenu2==6) {
 				System.out.println("Has terminado la primera parte");
-			default:
+			}else {
 				System.out.println("Error. No existe esa opcion");
 			}
-		} while (numMenu2 >=5 && numMenu2 <=1);
+			
+		} 
 	}
 	public static void imprimirMenuPersonal() {
 		System.out.println("  ***********MENÚ*******");
